667d5f6767bb91690ecbf75e182a921d8a001896
Mathieu Ablasou <alexherbo2@gmail.com>
2019-04-11 16:37:50 +0200

    Add The Lounge configuration
    https://thelounge.chat

---
+++ home/thelounge/config.js
@@ -0,0 +1,10 @@
+module.exports = {
+  defaults: {
+    name: 'freenode',
+    host: 'chat.freenode.net',
+    nick: 'alex``',
+    username: 'alexherbo2',
+    realname: 'Alex Leferry 2>',
+    join: '#kakoune,#mpv,#nixos'
+  }
+}

c1ecd43785c430df1015d712952e3940a97aa184
Mathieu Ablasou <alexherbo2@gmail.com>
2019-04-13 21:59:46 +0200

    The Lounge: Join #mpv-android
    https://github.com/mpv-android/mpv-android

--- home/thelounge/config.js
+++ home/thelounge/config.js
@@ -5,6 +5,6 @@ module.exports = {
     nick: 'alexherbo2',
     username: 'alexherbo2',
     realname: 'Alex Leferry 2>',
-    join: '#crystal-lang,#kakoune,#mpv,#nixos'
+    join: '#crystal-lang,#kakoune,#mpv,#mpv-android,#nixos'
   }
 }

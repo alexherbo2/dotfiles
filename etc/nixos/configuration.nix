53aee352f70e699937e179710ba820bb05f6b011
Mathieu Ablasou <alexherbo2@gmail.com>
2019-09-28 00:26:53 +0200

    NixOS: Re-enable VirtualBox

--- etc/nixos/configuration.nix
+++ etc/nixos/configuration.nix
@@ -188,7 +188,7 @@
 
   # Virtualisation
   virtualisation.docker.enable = true;
-  # virtualisation.virtualbox.host.enable = true;
+  virtualisation.virtualbox.host.enable = true;
 
   # Packages ───────────────────────────────────────────────────────────────────
 

31e891ff4d426bb0f5257f9afbb16590ba8b639f
Mathieu Ablasou <alexherbo2@gmail.com>
2019-10-29 15:01:52 +0100

    The Lounge: Join #html

--- home/thelounge/config.js
+++ home/thelounge/config.js
@@ -5,6 +5,6 @@ module.exports = {
     nick: 'alexherbo2',
     username: 'alexherbo2',
     realname: 'Alex Leferry 2>',
-    join: '#archlinux,#batch,#chromium,#chromium-extensions,#chromium-support,#crystal-lang,#css,#curl,#elvish,#evil-mode,#i3,##javascript,#kakoune,#krabby,#mpv,#mpv-android,#next-browser,#nixos,#qutebrowser,#thelounge,#tridactyl,#uzbl,#vim-fr'
+    join: '#archlinux,#batch,#chromium,#chromium-extensions,#chromium-support,#crystal-lang,#css,#curl,#elvish,#evil-mode,#html,#i3,##javascript,#kakoune,#krabby,#mpv,#mpv-android,#next-browser,#nixos,#qutebrowser,#thelounge,#tridactyl,#uzbl,#vim-fr'
   }
 }

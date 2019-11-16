406251e095c6380c16ae5aa7821db048329c90a0
Mathieu Ablasou <alexherbo2@gmail.com>
2019-11-16 18:17:47 +0100

    Krabby: Read Berserk with mpv
    https://readberserk.com

--- config/krabby/config.js
+++ config/krabby/config.js
@@ -1,1 +1,11 @@
 // Configuration for Krabby (https://github.com/alexherbo2/krabby/blob/master/src/krabby.js)
+
+const { extensions, modes } = krabby
+const { shell } = extensions
+const { modal } = modes
+
+modal.filter('Read Berserk', () => location.hostname === 'readberserk.com', 'Command')
+modal.filter('Read Berserk · Chapter', () => location.pathname.startsWith('/chapter'), 'Read Berserk')
+modal.enable('Read Berserk · Chapter', 'Read Berserk', ...modal.context.filters)
+
+modal.map('Read Berserk · Chapter', ['KeyM'], () => shell.send('mpv', ...Array.from(document.querySelectorAll('.pages__img'), (image) => image.src)), 'Read Berserk with mpv', 'Read Berserk · Chapter')

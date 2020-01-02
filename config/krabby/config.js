7050f0d8bb91cd6371a371de37f5eb0fb78651e6
Mathieu Ablasou <alexherbo2@gmail.com>
2020-01-02 14:24:40 +0100

    Krabby: Override individual configuration options of Alacritty

--- config/krabby/config.js
+++ config/krabby/config.js
@@ -4,7 +4,24 @@ const { env, extensions, modes } = krabby
 const { shell } = extensions
 const { modal } = modes
 
-env.EDITOR = 'alacritty --class alacritty-float --command kak "$1" -e "select $2.$3,$4.$5"'
+// Override individual configuration options of Alacritty with a temporary config file.
+env.EDITOR = `
+  # Environment variables
+  XDG_CONFIG_HOME=\${XDG_CONFIG_HOME:-~/.config}
+  CONFIG=$XDG_CONFIG_HOME/alacritty/alacritty.yml
+  # Create a temporary config file
+  config=$(mktemp)
+  trap "rm -f $config" EXIT
+  # Populate configuration if available
+  if test -f "$CONFIG"; then
+    cp "$CONFIG" "$config"
+  fi
+  # Additional settings
+  cat <<'EOF' >> "$config"
+background_opacity: 0.75
+EOF
+  alacritty --config-file "$config" --class alacritty-float --command kak "$1" -e "select $2.$3,$4.$5"
+`
 
 modal.filter('Read Berserk', () => location.hostname === 'readberserk.com', 'Command')
 modal.filter('Read Berserk · Chapter', () => location.pathname.startsWith('/chapter'), 'Read Berserk')

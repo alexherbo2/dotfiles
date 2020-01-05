99a5434ea3bd262ecbc5f78f09c2c0b6c0040c45
Mathieu Ablasou <alexherbo2@gmail.com>
2020-01-05 11:50:21 +0100

    The Lounge: Join #emacs
    https://gnu.org/software/emacs/

--- home/thelounge/config.js
+++ home/thelounge/config.js
@@ -5,6 +5,6 @@ module.exports = {
     nick: 'alexherbo2',
     username: 'alexherbo2',
     realname: 'Alex Leferry 2>',
-    join: '#alacritty,#archlinux,#batch,#bspwm,#chromium,#chromium-extensions,#chromium-support,#crystal-lang,#css,#curl,#elvish,#evil-mode,#ffmpeg,#freebsd,#freebsd-fr,#html,#i3,##javascript,#jq,#kakoune,#krabby,#lf,#mpv,#mpv-android,#neovim,#next-browser,#nixos,#qutebrowser,#ruby,#rubyonrails,#sway,#termux,#thelounge,#tmux,#tridactyl,#uzbl,#vim,#vim-fr'
+    join: '#alacritty,#archlinux,#batch,#bspwm,#chromium,#chromium-extensions,#chromium-support,#crystal-lang,#css,#curl,#elvish,#emacs,#evil-mode,#ffmpeg,#freebsd,#freebsd-fr,#html,#i3,##javascript,#jq,#kakoune,#krabby,#lf,#mpv,#mpv-android,#neovim,#next-browser,#nixos,#qutebrowser,#ruby,#rubyonrails,#sway,#termux,#thelounge,#tmux,#tridactyl,#uzbl,#vim,#vim-fr'
   }
 }

e4a192d38be3d2a74218ee42a2981c9a90c77d6f
Mathieu Ablasou <alexherbo2@gmail.com>
2020-02-03 04:35:18 +0100

    The Lounge: Join #chronic
    https://github.com/alexherbo2/chronic

--- home/thelounge/config.js
+++ home/thelounge/config.js
@@ -5,6 +5,6 @@ module.exports = {
     nick: 'alexherbo2',
     username: 'alexherbo2',
     realname: 'Alex Leferry 2>',
-    join: '#alacritty,#archlinux,#bash,#batch,#bspwc,#bspwm,#chromium,#chromium-extensions,#chromium-support,#crystal-lang,#css,#curl,#elvish,#emacs,#evil-mode,#ffmpeg,#freebsd,#freebsd-fr,#html,#i3,##javascript,#jq,#kakoune,#kde,#krabby,#lf,#mpv,#mpv-android,#neovim,#next-browser,#nixos,#password,#qutebrowser,#ruby,#rubyonrails,#sway,#termux,#thelounge,#tmux,#tridactyl,#uzbl,#vim,#vim-fr,#waybox,#wayfire'
+    join: '#alacritty,#archlinux,#bash,#batch,#bspwc,#bspwm,#chromium,#chromium-extensions,#chromium-support,#chronic,#crystal-lang,#css,#curl,#elvish,#emacs,#evil-mode,#ffmpeg,#freebsd,#freebsd-fr,#html,#i3,##javascript,#jq,#kakoune,#kde,#krabby,#lf,#mpv,#mpv-android,#neovim,#next-browser,#nixos,#password,#qutebrowser,#ruby,#rubyonrails,#sway,#termux,#thelounge,#tmux,#tridactyl,#uzbl,#vim,#vim-fr,#waybox,#wayfire'
   }
 }

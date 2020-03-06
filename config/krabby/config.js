07737be990b055065bf1cc6ad35a7737e7058059
Mathieu Ablasou <alexherbo2@gmail.com>
2020-03-06 16:09:29 +0100

    Krabby: Remove Berserk mappings

--- config/krabby/config.js
+++ config/krabby/config.js
@@ -47,9 +47,3 @@ dmenu.send('set', {
 editor.send('set', {
   editor: `alacritty --class 'Alacritty · Floating' --command kak "$file" -e "select $anchor_line.$anchor_column,$cursor_line.$cursor_column"`
 })
-
-modal.filter('Read Berserk', () => location.hostname === 'readberserk.com', 'Command')
-modal.filter('Read Berserk · Chapter', () => location.pathname.startsWith('/chapter'), 'Read Berserk')
-modal.enable('Read Berserk · Chapter', 'Read Berserk', ...modal.context.filters)
-
-modal.map('Read Berserk · Chapter', ['KeyM'], () => shell.send('mpv', ...Array.from(document.querySelectorAll('.pages__img'), (image) => image.src)), 'Read Berserk with mpv', 'Read Berserk · Chapter')

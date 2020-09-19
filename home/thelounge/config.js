13c8896b918b244859df8154f5697e015a3ca208
Mathieu Ablasou <alexherbo2@gmail.com>
2020-09-19 14:55:17 +0200

    Rename Alex Leferry 2> → Mathieu Ablasou

--- home/thelounge/config.js
+++ home/thelounge/config.js
@@ -4,7 +4,7 @@ module.exports = {
     host: 'chat.freenode.net',
     nick: 'alexherbo2',
     username: 'alexherbo2',
-    realname: 'Alex Leferry 2>',
+    realname: 'Mathieu Ablasou',
     join: '#alacritty,#amber,#archlinux,#bash,#batch,#bspwc,#bspwm,#chromium,#chromium-extensions,#chromium-support,#chronic,#crystal-lang,#css,#curl,#elvish,#emacs,#evil-mode,#ffmpeg,#freebsd,#freebsd-fr,#html,#i3,#inkscape,##javascript,#jq,#kakoune,#kde,#krabby,#launcher,#lua,#mpv,#mpv-android,#neovim,#nixos,#password,#ruby,#rubyonrails,#svg,#sway,#termux,#thelounge,#tmux,#vim,#vim-fr,#waybox,#wayfire'
   }
 }

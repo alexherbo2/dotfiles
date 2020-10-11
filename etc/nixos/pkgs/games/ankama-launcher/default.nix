63d70e145d524436360045ce3a9948163d834d31
Mathieu Ablasou <alexherbo2@gmail.com>
2020-10-11 23:22:16 +0200

    NixOS: Update sha256 of ankama-launcher

--- etc/nixos/pkgs/games/ankama-launcher/default.nix
+++ etc/nixos/pkgs/games/ankama-launcher/default.nix
@@ -10,7 +10,7 @@ appimageTools.wrapType2 rec {
   src = fetchurl {
     name = "${pname}";
     url = "https://launcher.cdn.ankama.com/installers/production/Ankama%20Launcher-Setup-x86_64.AppImage";
-    sha256 = "1wkhhcmz6lmfgiy7hwpm9nbfxrky414ivhr4kc0g3frj3647vlzk";
+    sha256 = "17xrrcgrf1jnpy6717223ca036yzfcihnrspl1wm5jc51vc7k7sw";
   };
 
   extraInstallCommands = "mv $out/bin/${name} $out/bin/${pname}";

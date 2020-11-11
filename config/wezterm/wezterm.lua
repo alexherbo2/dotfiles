8450e3b0e59e40d082f9fb0eea1747c8ad502a0b
Mathieu Ablasou <alexherbo2@gmail.com>
2020-11-12 00:06:03 +0100

    Add WezTerm configuration
    https://wezfurlong.org/wezterm/

---
+++ config/wezterm/wezterm.lua
@@ -0,0 +1,9 @@
+local wezterm = require 'wezterm'
+
+return {
+  default_prog = { 'elvish' },
+  color_scheme = 'Dracula',
+  font = wezterm.font('PragmataPro'),
+  font_size = 11.0,
+  enable_csi_u_key_encoding = true
+}

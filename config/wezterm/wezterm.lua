94086bc1ad9ebe90f440187678cc49219fa78dc8
Mathieu Ablasou <alexherbo2@gmail.com>
2020-11-12 03:23:33 +0100

    WezTerm: Document the configuration

--- config/wezterm/wezterm.lua
+++ config/wezterm/wezterm.lua
@@ -1,13 +1,37 @@
+-- WezTerm
+-- https://wezfurlong.org/wezterm/
+
 local wezterm = require 'wezterm'
 
 return {
+  -- Shell
+  -- https://wezfurlong.org/wezterm/config/launch.html
+  --
+  -- Elvish
+  -- https://elv.sh
   default_prog = { 'elvish' },
+
+  -- Color scheme
+  -- https://wezfurlong.org/wezterm/config/appearance.html
+  --
+  -- Dracula
+  -- https://draculatheme.com
   color_scheme = 'Dracula',
+
+  -- Font configuration
+  -- https://wezfurlong.org/wezterm/config/fonts.html
   font = wezterm.font('PragmataPro'),
   font_size = 11.0,
+
   -- Disable ligatures
   -- https://wezfurlong.org/wezterm/config/font-shaping.html
   harfbuzz_features = { 'calt=0', 'clig=0', 'liga=0' },
+
+  -- Enable CSI u mode
+  -- https://wezfurlong.org/wezterm/changelog.html?highlight=enable_csi_u_key_encoding#20200620-160318-e00b076c
   enable_csi_u_key_encoding = true,
+
+  -- Wayland
+  -- https://wezfurlong.org/wezterm/config/misc.html
   enable_wayland = true
 }

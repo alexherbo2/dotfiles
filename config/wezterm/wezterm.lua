d1520bb5b75de08e1245e2d016f7fc52ed580041
Mathieu Ablasou <alexherbo2@gmail.com>
2020-11-12 02:56:05 +0100

    WezTerm: Disable ligatures
    https://wezfurlong.org/wezterm/config/font-shaping.html

--- config/wezterm/wezterm.lua
+++ config/wezterm/wezterm.lua
@@ -5,5 +5,8 @@ return {
   color_scheme = 'Dracula',
   font = wezterm.font('PragmataPro'),
   font_size = 11.0,
+  -- Disable ligatures
+  -- https://wezfurlong.org/wezterm/config/font-shaping.html
+  harfbuzz_features = { 'calt=0', 'clig=0', 'liga=0' },
   enable_csi_u_key_encoding = true
 }

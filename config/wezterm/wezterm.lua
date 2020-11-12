3ba82ad6efe2f4cd9053e9662b655bb67b5b1fbf
Mathieu Ablasou <alexherbo2@gmail.com>
2020-11-12 02:58:01 +0100

    WezTerm: Enable Wayland

--- config/wezterm/wezterm.lua
+++ config/wezterm/wezterm.lua
@@ -8,5 +8,6 @@ return {
   -- Disable ligatures
   -- https://wezfurlong.org/wezterm/config/font-shaping.html
   harfbuzz_features = { 'calt=0', 'clig=0', 'liga=0' },
-  enable_csi_u_key_encoding = true
+  enable_csi_u_key_encoding = true,
+  enable_wayland = true
 }

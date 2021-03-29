f968f26bf180e10cffcf725d69f28e2d7231d03b
Mathieu Ablasou <alexherbo2@gmail.com>
2021-03-29 14:08:52 +0200

    Remove bspwm

--- home/thelounge/config.js
+++ home/thelounge/config.js
@@ -5,6 +5,6 @@ module.exports = {
     nick: 'alexherbo2',
     username: 'alexherbo2',
     realname: 'Mathieu Ablasou',
-    join: '#alacritty,#amber,#archlinux,#bash,#batch,#bspwc,#bspwm,#chromium,#chromium-extensions,#chromium-support,#chronic,#crystal-lang,#css,#curl,#emacs,#evil-mode,#ffmpeg,#freebsd,#freebsd-fr,#html,#i3,#inkscape,##javascript,#jq,#kakoune,#kde,#krabby,#launcher,#lua,#mpv,#mpv-android,#neovim,#nixos,#password,#ruby,#rubyonrails,#svg,#sway,#termux,#thelounge,#tmux,#vim,#vim-fr'
+    join: '#alacritty,#amber,#archlinux,#bash,#batch,#bspwc,#chromium,#chromium-extensions,#chromium-support,#chronic,#crystal-lang,#css,#curl,#emacs,#evil-mode,#ffmpeg,#freebsd,#freebsd-fr,#html,#i3,#inkscape,##javascript,#jq,#kakoune,#kde,#krabby,#launcher,#lua,#mpv,#mpv-android,#neovim,#nixos,#password,#ruby,#rubyonrails,#svg,#sway,#termux,#thelounge,#tmux,#vim,#vim-fr'
   }
 }

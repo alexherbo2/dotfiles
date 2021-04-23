d2823e19fdba5e194a2480b5b6f283c27e9e5fac
Mathieu Ablasou <alexherbo2@gmail.com>
2021-04-23 21:39:59 +0200

    The Lounge: Join #rspec
    https://rspec.info

--- home/thelounge/config.js
+++ home/thelounge/config.js
@@ -5,6 +5,6 @@ module.exports = {
     nick: 'alexherbo2',
     username: 'alexherbo2',
     realname: 'Mathieu Ablasou',
-    join: '#alacritty,#amber,#archlinux,#bash,#batch,#chromium,#chromium-extensions,#chromium-support,#chronic,#crystal-lang,#css,#curl,#ffmpeg,#freebsd,#freebsd-fr,#html,#inkscape,##javascript,#jq,#kakoune,#kde,#krabby,#lua,#mpv,#mpv-android,#nixos,#ruby,#rubyonrails,#svg,#sway,#termux,#thelounge'
+    join: '#alacritty,#amber,#archlinux,#bash,#batch,#chromium,#chromium-extensions,#chromium-support,#chronic,#crystal-lang,#css,#curl,#ffmpeg,#freebsd,#freebsd-fr,#html,#inkscape,##javascript,#jq,#kakoune,#kde,#krabby,#lua,#mpv,#mpv-android,#nixos,#rspec,#ruby,#rubyonrails,#svg,#sway,#termux,#thelounge'
   }
 }

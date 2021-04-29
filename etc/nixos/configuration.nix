906824e3376c5d5d9642fa43a6f2eb4f9ffd7905
Mathieu Ablasou <alexherbo2@gmail.com>
2021-04-29 20:38:22 +0200

    NixOS: Disable PostgreSQL

--- etc/nixos/configuration.nix
+++ etc/nixos/configuration.nix
@@ -199,9 +199,9 @@
   # PostgreSQL
   # https://postgresql.org
   # https://nixos.org/manual/nixos/stable#module-postgresql
-  services.postgresql.enable = true;
-  services.postgresql.package = pkgs.postgresql_13;
-  services.postgresql.dataDir = "/data/postgresql";
+  # services.postgresql.enable = true;
+  # services.postgresql.package = pkgs.postgresql_13;
+  # services.postgresql.dataDir = "/data/postgresql";
 
   # Packages ───────────────────────────────────────────────────────────────────
 

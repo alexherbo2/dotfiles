ffb1085c404ae4cd70a6ee9ec62ba079d17894b4
Mathieu Ablasou <alexherbo2@gmail.com>
2021-10-23 00:38:53 +0200

    Add Emacs
    https://gnu.org/software/emacs/

---
+++ home/emacs.d/init.el
@@ -0,0 +1,16 @@
+;; Emacs
+;; https://gnu.org/software/emacs/
+
+(tool-bar-mode -1)
+
+;; Evil
+;; https://github.com/emacs-evil/evil
+(add-to-list 'load-path "~/repositories/github.com/emacs-evil/evil")
+(add-to-list 'load-path "~/repositories/github.com/emacs-evil/goto-chg")
+(require 'evil)
+(evil-mode 1)
+
+;; Dracula
+;; https://draculatheme.com/emacs
+(add-to-list 'custom-theme-load-path "~/repositories/github.com/dracula/emacs")
+(load-theme 'dracula t)

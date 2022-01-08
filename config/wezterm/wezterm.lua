846dca2679e67d257709361bcd8aeaab217cb9de
Mathieu Ablasou <alexherbo2@gmail.com>
2022-01-08 23:07:29 +0100

    WezTerm: Change cursor style

--- config/wezterm/wezterm.lua
+++ config/wezterm/wezterm.lua
@@ -22,6 +22,9 @@ return {
   -- https://wezfurlong.org/wezterm/config/font-shaping.html
   harfbuzz_features = { 'calt=0', 'clig=0', 'liga=0' },
 
+  -- Cursor style
+  default_cursor_style = 'BlinkingBar',
+
   -- Enable CSI u mode
   -- https://wezfurlong.org/wezterm/config/lua/config/enable_csi_u_key_encoding.html
   enable_csi_u_key_encoding = true

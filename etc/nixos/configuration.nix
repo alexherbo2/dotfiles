7217b398732c7e980270394de948cc2ee003c52d
Mathieu Ablasou <taupiqueur.kanto@gmail.com>
2022-01-12 18:27:20 +0100

    NixOS: Update stateVersion

--- etc/nixos/configuration.nix
+++ etc/nixos/configuration.nix
@@ -15,7 +15,7 @@
   # This value determines the NixOS release with which your system is to be compatible,
   # in order to avoid breaking some software such as database servers.
   # You should change this only after NixOS release notes say you should.
-  system.stateVersion = "21.11";
+  system.stateVersion = "22.05";
 
   # Flakes ─────────────────────────────────────────────────────────────────────
 

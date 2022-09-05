59724ce3b7034a36a79c0edb02084ca88793a247
Mathieu Ablasou <alexherbo2@gmail.com>
2022-09-05 15:05:35 +0200

    2022-09-05 15:05:35

--- config/wezterm/wezterm.lua
+++ config/wezterm/wezterm.lua
@@ -4,60 +4,71 @@
 local wezterm = require 'wezterm'
 
 return {
+  -- Appearance ----------------------------------------------------------------
+
   -- Smart tab bar [distraction-free mode]
   hide_tab_bar_if_only_one_tab = true,
 
   -- Color scheme
-  -- https://wezfurlong.org/wezterm/config/appearance.html
-  --
-  -- Catppuccin
-  -- https://catppuccin.com
   color_scheme = 'Catppuccin Frappe',
 
+  -- Add a bit of transparency to reduce eye strain.
   window_background_opacity = 0.99,
 
   -- Font configuration
-  -- https://wezfurlong.org/wezterm/config/fonts.html
   font = wezterm.font('JetBrains Mono'),
   font_size = 16.0,
 
   -- Disable ligatures
-  -- https://wezfurlong.org/wezterm/config/font-shaping.html
   harfbuzz_features = { 'calt=0', 'clig=0', 'liga=0' },
 
   -- Cursor style
   default_cursor_style = 'BlinkingBar',
 
+  -- Key bindings --------------------------------------------------------------
+
   -- Leader key
-  -- https://wezfurlong.org/wezterm/config/keys.html#leader-key
-  leader = { key = '`', timeout_milliseconds = 9000 },
+  leader = { key = [[`]], timeout_milliseconds = 9000 },
 
   -- Key bindings
-  -- https://wezfurlong.org/wezterm/config/keys.html
   keys = {
-    -- Send "`" to the terminal when pressing "`", "`".
-    { key = '`', mods="LEADER", action=wezterm.action{SendString='`'} },
-    { key = 'o', mods="LEADER", action=wezterm.action{SplitVertical={domain="CurrentPaneDomain"}} },
-    { key = 'O', mods="LEADER", action=wezterm.action{SplitHorizontal={domain="CurrentPaneDomain"}} },
-    { key = 'c', mods="LEADER", action=wezterm.action{SpawnTab="CurrentPaneDomain"} },
-    { key = 'h', mods="LEADER", action=wezterm.action{ActivatePaneDirection="Left"} },
-    { key = 'l', mods="LEADER", action=wezterm.action{ActivatePaneDirection="Right"} },
-    { key = 'k', mods="LEADER", action=wezterm.action{ActivatePaneDirection="Up"} },
-    { key = 'j', mods="LEADER", action=wezterm.action{ActivatePaneDirection="Down"} },
-    { key = 'f', mods="LEADER", action="TogglePaneZoomState" },
-    { key = 'v', mods="LEADER", action="ActivateCopyMode" },
-    { key = '1', mods="LEADER", action=wezterm.action{ActivateTab=0} },
-    { key = '2', mods="LEADER", action=wezterm.action{ActivateTab=1} },
-    { key = '3', mods="LEADER", action=wezterm.action{ActivateTab=2} },
-    { key = '4', mods="LEADER", action=wezterm.action{ActivateTab=3} },
-    { key = '5', mods="LEADER", action=wezterm.action{ActivateTab=4} },
-    { key = '6', mods="LEADER", action=wezterm.action{ActivateTab=5} },
-    { key = '7', mods="LEADER", action=wezterm.action{ActivateTab=6} },
-    { key = '8', mods="LEADER", action=wezterm.action{ActivateTab=7} },
-    { key = '9', mods="LEADER", action=wezterm.action{ActivateTab=-1} },
-    { key = 'n', mods="LEADER", action=wezterm.action{ActivateTabRelative=1} },
-    { key = 'p', mods="LEADER", action=wezterm.action{ActivateTabRelative=-1} },
-    { key = 'N', mods="LEADER", action=wezterm.action{MoveTabRelative=1} },
-    { key = 'P', mods="LEADER", action=wezterm.action{MoveTabRelative=-1} },
+    -- Send keys
+    { key = [[`]], mods = 'LEADER', action = wezterm.action.SendString [[`]] },
+
+    -- Clipboard
+    { key = 'v', mods = 'LEADER', action = wezterm.action.ActivateCopyMode },
+
+    -- Zoom
+    { key = 'f', mods = 'LEADER', action = wezterm.action.TogglePaneZoomState },
+
+    -- Create tabs
+    { key = 'c', mods = 'LEADER', action = wezterm.action.SpawnTab 'CurrentPaneDomain' },
+
+    -- Split panes
+    { key = 'o', mods = 'LEADER', action = wezterm.action.SplitVertical { domain = 'CurrentPaneDomain' } },
+    { key = 'O', mods = 'LEADER', action = wezterm.action.SplitHorizontal { domain = 'CurrentPaneDomain' } },
+
+    -- Focus panes
+    { key = 'h', mods = 'LEADER', action = wezterm.action.ActivatePaneDirection 'Left' },
+    { key = 'j', mods = 'LEADER', action = wezterm.action.ActivatePaneDirection 'Down' },
+    { key = 'k', mods = 'LEADER', action = wezterm.action.ActivatePaneDirection 'Up' },
+    { key = 'l', mods = 'LEADER', action = wezterm.action.ActivatePaneDirection 'Right' },
+
+    -- Switch tabs
+    { key = 'n', mods = 'LEADER', action = wezterm.action.ActivateTabRelative(1) },
+    { key = 'p', mods = 'LEADER', action = wezterm.action.ActivateTabRelative(-1) },
+    { key = '1', mods = 'LEADER', action = wezterm.action.ActivateTab(0) },
+    { key = '2', mods = 'LEADER', action = wezterm.action.ActivateTab(1) },
+    { key = '3', mods = 'LEADER', action = wezterm.action.ActivateTab(2) },
+    { key = '4', mods = 'LEADER', action = wezterm.action.ActivateTab(3) },
+    { key = '5', mods = 'LEADER', action = wezterm.action.ActivateTab(4) },
+    { key = '6', mods = 'LEADER', action = wezterm.action.ActivateTab(5) },
+    { key = '7', mods = 'LEADER', action = wezterm.action.ActivateTab(6) },
+    { key = '8', mods = 'LEADER', action = wezterm.action.ActivateTab(7) },
+    { key = '9', mods = 'LEADER', action = wezterm.action.ActivateTab(-1) },
+
+    -- Move tabs
+    { key = 'N', mods = 'LEADER', action = wezterm.action.MoveTabRelative(1) },
+    { key = 'P', mods = 'LEADER', action = wezterm.action.MoveTabRelative(-1) },
   }
 }

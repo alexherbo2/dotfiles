0a456bec7bfb4d2921a6d934dbd7fca21532e9ca
Mathieu Ablasou <alexherbo2@gmail.com>
2022-10-13 22:48:52 +0200

    2022-10-13 22:48:52

--- .config/wezterm/wezterm.lua
+++ .config/wezterm/wezterm.lua
@@ -13,9 +13,6 @@ return {
 
   -- Appearance ----------------------------------------------------------------
 
-  -- Remove window decorations
-  window_decorations = 'RESIZE',
-
   -- Tab bar appearance and colors
   use_fancy_tab_bar = false,
   hide_tab_bar_if_only_one_tab = true,

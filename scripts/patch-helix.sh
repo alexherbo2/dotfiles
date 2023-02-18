6f40e6ba8fa19ceeb415347566e34c664a376aa3
Mathieu Ablasou <taupiqueur.kanto@gmail.com>
2023-02-18 12:03:18 +0100

    2023-02-18 12:03:18

--- scripts/patch-helix.sh
+++
@@ -1,9 +0,0 @@
-# https://github.com/helix-editor/helix/issues/2111
-cat <<EOF >> Cargo.toml
-[patch.crates-io]
-crossterm = { git = "https://github.com/yyogo/crossterm.git", branch = "use-select-in-unix" }
-EOF
-
-# https://github.com/helix-editor/helix/pull/3328
-curl -L https://github.com/helix-editor/helix/pull/3328.patch |
-git apply -3

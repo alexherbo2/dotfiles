6c39e0a513af8e124a68e412d56d1ad0aa368e7c
Mathieu Ablasou <alexherbo2@gmail.com>
2023-12-19 21:48:07 +0100

    2023-12-19 21:48:07

--- .config/kak/scripts/create_jump_state.cr
+++ .config/kak/scripts/create_jump_state.cr
@@ -4,9 +4,12 @@ kak_opt_jump_charset, *kak_selections_desc = ARGV
 jump_ranges = [] of String
 jump_label_selection_map = {} of String => String
 
-kak_selections_desc.each_with_index do |selection_desc, index|
-  jump_label = kak_opt_jump_charset[index // kak_opt_jump_charset.size].to_s + kak_opt_jump_charset[index % kak_opt_jump_charset.size]
-  anchor_desc, cursor_desc = selection_desc.split(',')
+kak_selections_desc.each_with_index.first(kak_opt_jump_charset.size ** 2).each do |selection_desc, index|
+  jump_label = "%c%c" % [
+    kak_opt_jump_charset[index // kak_opt_jump_charset.size],
+    kak_opt_jump_charset[index % kak_opt_jump_charset.size]
+  ]
+  anchor_desc = selection_desc.split(',').first
   jump_ranges << "#{anchor_desc}+2|{JumpLabel}#{jump_label}"
   jump_label_selection_map[jump_label] = selection_desc
 end

fd4069383bc460e845c39efa4e4756b500a8b4ab
Mathieu Ablasou <alexherbo2@gmail.com>
2026-09-10 12:29:42 +0200

    2026-09-10 12:29:42

---
+++ .local/share/kak/assets/editor.sh
@@ -0,0 +1,40 @@
+#!/bin/sh
+set -e
+line_number=0
+while :
+do
+  case "$1" in
+    "+"|"+"[0-9]*)
+      line_number="${1#+}"
+      shift
+      ;;
+    "--")
+      shift
+      break
+      ;;
+    "-"*)
+      echo "ERROR: $1 is not a valid option." >&2
+      exit 1
+      ;;
+    *)
+      break
+      ;;
+  esac
+done
+if [ "$#" -eq 0 ]
+then exit 1
+fi
+kak_client_init=
+if [ -z "$line_number" ]
+then kak_client_init="exec -client '$kak_client' 'gj'"
+elif [ "$cursor_line" -gt 0 ]
+then kak_client_init="exec -client '$kak_client' -- '${line_number}g'"
+fi
+realpath -- "$@" |
+sed "1!G;h;\$!d;s/'/''/g" |
+tr "\\n" "\\0" |
+{
+  xargs -0 printf "eval -client '$kak_client' -verbatim edit -- '%s'\\n"
+  printf "%s\\n" "$kak_client_init"
+} |
+kak -p "$kak_session"

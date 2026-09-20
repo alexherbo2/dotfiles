4685d9e596fca15fd06b9b43ea6f9b94b159807f
Mathieu Ablasou <alexherbo2@gmail.com>
2026-09-20 20:47:54 +0200

    2026-09-20 20:47:54

--- .local/share/kak/assets/kakoune/shell-completion/kak.zsh
+++ .local/share/kak/assets/kakoune/shell-completion/kak.zsh
@@ -1,32 +1,38 @@
 #compdef kak
 
 local OPTION_WORDLIST=(
-  "-p[Specifies the command to run on unchanged items]:command:_command_names"
-  "--pick-command=[Specifies the command to run on unchanged items]:command:_command_names"
-  "-m[Specifies the command to run on modified items]:command:_command_names"
-  "--map-command=[Specifies the command to run on modified items]:command:_command_names"
-  "-d[Specifies the command to run on deleted items]:command:_command_names"
-  "--drop-command=[Specifies the command to run on deleted items]:command:_command_names"
-  "-P[Specifies the shell script to run on unchanged items]:command:_command_names"
-  "--pick-shell-script=[Specifies the shell script to run on unchanged items]:command:_command_names"
-  "-M[Specifies the shell script to run on modified items]:command:_command_names"
-  "--map-shell-script=[Specifies the shell script to run on modified items]:command:_command_names"
-  "-D[Specifies the shell script to run on deleted items]:command:_command_names"
-  "--drop-shell-script=[Specifies the shell script to run on deleted items]:command:_command_names"
-  "-e[Specifies the editor to use]:command:_command_names"
-  "--editor=[Specifies the editor to use]:command:_command_names"
-  "*-f[Adds a filter (repeatable)]:command:_command_names"
-  "*--filter=[Adds a filter (repeatable)]:command:_command_names"
-  "-E[Do not open editor]"
-  "--no-edit[Do not open editor]"
-  "--no-pick[Do not pick items]"
-  "--no-map[Do not map items]"
-  "--no-drop[Do not drop items]"
-  "-[Read items from stdin]"
-  "-h[Show this help]"
-  "--help[Show this help]"
-  "-V[Show version]"
-  "--version[Show version]"
+  "-c[connect to session]:session_name:_kak_session_names"
+  "-C[connect or create session]:session_name:_kak_session_names"
+  "-e[execute commands on client initialization]:commands:"
+  "-E[execute commands on server initialization]:commands:"
+  "-n[ignore kakrc]"
+  "-s[set session name]:session_name:"
+  "-d[run in headless mode]"
+  "-p[send commands to session]:session_name:_kak_session_names"
+  "-f[run in filter mode]:keys:"
+  "-i[edit files in-place, saving backups with the specified extension]:keys:"
+  "-q[run in quiet mode]"
+  "-ui[set the type of user interface]:ui_type:_kak_ui_types"
+  "-ro[run in read-only mode]"
+  "-l[list sessions]"
+  "-clear[clear dead sessions]"
+  "-debug[set debug flags]:debug_flags:"
+  "-help[show this help]"
+  "-version[show version]"
 )
+
+local UI_TYPE_WORDLIST=(
+  "terminal[terminal]"
+  "dummy[dummy]"
+  "json[json]"
+)
+
+_kak_session_names() {
+  compadd - $(kak -l)
+}
+
+_kak_ui_types() {
+  _values "ui_type" "${UI_TYPE_WORDLIST[@]}"
+}
 
 _arguments -S -s "${OPTION_WORDLIST[@]}" "*:file:_files"
